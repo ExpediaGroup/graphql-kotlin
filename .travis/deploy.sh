e881c46c15b69441e40227335177b1ac4518ec34
Brennan Taylor <taylor.brennan+github@gmail.com>
2018-10-03 16:01:55 -0700

    Ignore pom.xml changes when preparing for release (#29)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,7 +17,6 @@ fi
 
 echo "travis tag is set -> updating pom.xml <version> attribute to $TRAVIS_TAG"
 ./mvnw --batch-mode release:update-versions -DdevelopmentVersion=$TRAVIS_TAG-SNAPSHOT
-git commit -am "setting version to $TRAVIS_TAG-SNAPSHOT"
-./mvnw --batch-mode --settings .travis/settings.xml --no-snapshot-updates -Prelease -DskipTests=true -DreleaseVersion=$TRAVIS_TAG release:prepare
+./mvnw --batch-mode --settings .travis/settings.xml --no-snapshot-updates -Prelease -DskipTests=true -DreleaseVersion=$TRAVIS_TAG -DcheckModificationExcludeList=pom.xml release:prepare
 ./mvnw --batch-mode --settings .travis/settings.xml -Prelease clean deploy -DskipTests=true -B -U
 echo "successfully deployed the jars to nexus"
