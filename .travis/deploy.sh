b47a95f4f1aab8607fff88ee8b8336782d2bf5f6
Brennan Taylor <brentaylor@expedia.com>
2018-10-04 14:42:04 -0700

    prepare for next release

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,23 +1,28 @@
-#!/bin/bash -ex
-cd `dirname $0`/..
+#!/bin/bash -x
+cd `dirname $0`/.. 
 
-echo TRAVIS_BRANCH=$TRAVIS_BRANCH
-echo TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST
-echo TRAVIS_TAG=$TRAVIS_TAG
-
-if [ "$TRAVIS_BRANCH" == 'master' ] || [ "$TRAVIS_PULL_REQUEST" == 'true' ] || [ -z "$TRAVIS_TAG" ]; then
-    echo "Skipping artifact deployment for a non-release build"
-    exit 0
-fi
-
-if [[ -z "$SONATYPE_USERNAME" || -z "$SONATYPE_PASSWORD" ]]; then
-    echo "ERROR! Please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+if [ -z "$SONATYPE_USERNAME" ]
+then
+    echo "ERROR! Please set SONATYPE_USERNAME environment variable"
     exit 1
 fi
 
-echo "travis tag is set -> updating pom.xml <version> attribute to $TRAVIS_TAG"
-./mvnw --batch-mode release:update-versions -DdevelopmentVersion=$TRAVIS_TAG-SNAPSHOT
-git checkout -b release-$TRAVIS_TAG
-./mvnw --batch-mode --settings .travis/settings.xml --no-snapshot-updates -Prelease -DpushChanges=false -DskipTests=true -DreleaseVersion=$TRAVIS_TAG -DcheckModificationExcludeList=pom.xml release:prepare
-./mvnw --batch-mode --settings .travis/settings.xml -Prelease clean deploy -DpushChanges=false -DskipTests=true -B -U
+if [ -z "$SONATYPE_PASSWORD" ]
+then
+    echo "ERROR! Please set SONATYPE_PASSWORD environment variable"
+    exit 1
+fi
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    SKIP_GPG_SIGN=false
+    echo "travis tag is set -> updating pom.xml <version> attribute to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+else
+    SKIP_GPG_SIGN=true
+    echo "no travis tag is set, hence keeping the snapshot version in pom.xml"
+fi
+
+mvn clean deploy --settings .travis/settings.xml -Dgpg.skip=$SKIP_GPG_SIGN -DskipTests=true -B -U
+
 echo "successfully deployed the jars to nexus"
