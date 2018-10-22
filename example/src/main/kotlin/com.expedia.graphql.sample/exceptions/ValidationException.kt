62395420c49ed25abc468e15707d3fa2bf656793
Guillaume Scheibel <gscheibel@expedia.com>
2018-10-22 10:15:41 -0500

    Moving ConstraintError instantiation to an extension fucntion

--- example/src/main/kotlin/com.expedia.graphql.sample/exceptions/ValidationException.kt
+++ example/src/main/kotlin/com.expedia.graphql.sample/exceptions/ValidationException.kt
@@ -1,13 +1,13 @@
 package com.expedia.graphql.sample.exceptions
 
-import java.lang.RuntimeException
 import javax.validation.ConstraintViolation
-
 
 class ValidationException(val constraintErrors: List<ConstraintError>) : RuntimeException("Validation error")
 
-data class ConstraintError(private val constraintViolation: ConstraintViolation<*>,
-                           val path: String = constraintViolation.propertyPath.toString(),
-                           val message: String = constraintViolation.message,
-                           val type: String = constraintViolation.leafBean.toString()
+data class ConstraintError(val path: String, val message: String, val type: String)
+
+fun ConstraintViolation<*>.asConstraintError() = ConstraintError(
+    path = this.propertyPath.toString(),
+    message = this.message,
+    type = this.leafBean.toString()
 )
--- example/src/main/kotlin/com.expedia.graphql.sample/validation/DataFetcherExecutionValidator.kt
+++ example/src/main/kotlin/com.expedia.graphql.sample/validation/DataFetcherExecutionValidator.kt
@@ -1,7 +1,7 @@
 package com.expedia.graphql.sample.validation
 
-import com.expedia.graphql.sample.exceptions.ConstraintError
 import com.expedia.graphql.sample.exceptions.ValidationException
+import com.expedia.graphql.sample.exceptions.asConstraintError
 import com.expedia.graphql.schema.Parameter
 import com.expedia.graphql.schema.hooks.DataFetcherExecutionPredicate
 import graphql.schema.DataFetchingEnvironment
@@ -22,7 +22,7 @@ class DataFetcherExecutionValidator(private val validator: Validator?) : DataFet
 
     override fun onFailure(result: Any, parameter: Parameter, argumentName: String, environment: DataFetchingEnvironment): Nothing {
         val violations = result as Set<ConstraintViolation<*>>
-        throw ValidationException(violations.map { ConstraintError(it) })
+        throw ValidationException(violations.map { it.asConstraintError() })
     }
 
     override fun test(result: Any): Boolean = result is Set<*> && result.isEmpty()
