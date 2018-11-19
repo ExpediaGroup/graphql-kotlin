ee54ebe8f07635db2fb462cf294c12169dcf0cf3
darken <darken@darken.eu>
2018-11-19 23:35:09 +0100

    Remove unused file

--- src/main/kotlin/com/expedia/graphql/schema/generator/WiringContext.kt
+++
@@ -1,9 +0,0 @@
-package com.expedia.graphql.schema.generator
-
-/**
- * Schema generation related information
- */
-@Suppress("Detekt.EmptyClassBlock")
-class WiringContext internal constructor() {
-    // TODO Provide analogs to `SchemaDirectiveWiringEnvironment.java`, see ExpediaDotCom/graphql-kotlin/issues/60
-}
