a3f4dc8f5a2a0dd6865e72f25d41f5b3a17ffb83
Matthias Urhahn <darken@darken.eu>
2018-11-28 19:09:16 +0100

    Add missing description & deprecation annotation processing for enum classes and values. (#77)
    Closes #76

--- src/test/kotlin/com/expedia/graphql/schema/generator/types/EnumTypeTest.kt
+++ src/test/kotlin/com/expedia/graphql/schema/generator/types/EnumTypeTest.kt
@@ -1,12 +1,20 @@
 package com.expedia.graphql.schema.generator.types
 
+import com.expedia.graphql.annotations.GraphQLDescription
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 internal class EnumTypeTest {
-
+    @GraphQLDescription("MyTestEnum description")
     private enum class MyTestEnum {
+        @GraphQLDescription("enum 'ONE' description")
         ONE,
+
+        @Deprecated("Deprecated enum value")
         TWO
     }
 
@@ -17,5 +25,27 @@ internal class EnumTypeTest {
         assertEquals(expected = "MyTestEnum", actual = actual.name)
         assertEquals(expected = "ONE", actual = actual.values[0].value)
         assertEquals(expected = "TWO", actual = actual.values[1].value)
+    }
+
+    @Test
+    fun `Description on enum class and values`() {
+        val gqlEnum = assertNotNull(enumType(MyTestEnum::class))
+        assertEquals("MyTestEnum description", gqlEnum.description)
+
+        val one = assertNotNull(gqlEnum.getValue("ONE"))
+        assertEquals("enum 'ONE' description", one.description)
+    }
+
+    @Test
+    fun `Deprecation on enum values`() {
+        val gqlEnum = assertNotNull(enumType(MyTestEnum::class))
+
+        val one = assertNotNull(gqlEnum.getValue("ONE"))
+        assertFalse(one.isDeprecated)
+        assertNull(one.deprecationReason)
+
+        val two = assertNotNull(gqlEnum.getValue("TWO"))
+        assertTrue(two.isDeprecated)
+        assertEquals("Deprecated enum value", two.deprecationReason)
     }
 }
