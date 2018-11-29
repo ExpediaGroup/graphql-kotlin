c73c9d37d62884c03d2c2c50ea9ff639ba2f415e
Shane Myrick <mail@shanemyrick.com>
2018-11-28 16:16:39 -0800

    refactor: clean up names and unused arguments

--- src/main/kotlin/com/expedia/graphql/schema/extensions/kClassExtensions.kt
+++ src/main/kotlin/com/expedia/graphql/schema/extensions/kClassExtensions.kt
@@ -2,23 +2,20 @@ package com.expedia.graphql.schema.extensions
 
 import com.expedia.graphql.schema.generator.functionFilters
 import com.expedia.graphql.schema.generator.propertyFilters
-import com.expedia.graphql.schema.hooks.NoopSchemaGeneratorHooks
 import com.expedia.graphql.schema.hooks.SchemaGeneratorHooks
 import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.full.declaredMemberProperties
 
-private val noopHooks = NoopSchemaGeneratorHooks()
-
-internal fun KClass<*>.getValidProperties(hooks: SchemaGeneratorHooks = noopHooks) = this.declaredMemberProperties
+internal fun KClass<*>.getValidProperties(hooks: SchemaGeneratorHooks) = this.declaredMemberProperties
     .filter { hooks.isValidProperty(it) }
     .filter { prop -> propertyFilters.all { it.invoke(prop) } }
 
-internal fun KClass<*>.getValidFunctions(hooks: SchemaGeneratorHooks = noopHooks) = this.declaredMemberFunctions
+internal fun KClass<*>.getValidFunctions(hooks: SchemaGeneratorHooks) = this.declaredMemberFunctions
     .filter { hooks.isValidFunction(it) }
     .filter { func -> functionFilters.all { it.invoke(func) } }
 
-internal fun KClass<*>.canBeGraphQLInterface(): Boolean = this.java.isInterface
+internal fun KClass<*>.isGraphQLInterface(): Boolean = this.java.isInterface
 
-internal fun KClass<*>.canBeGraphQLUnion(): Boolean =
-    this.canBeGraphQLInterface() && this.declaredMemberProperties.isEmpty() && this.declaredMemberFunctions.isEmpty()
+internal fun KClass<*>.isGraphQLUnion(): Boolean =
+    this.isGraphQLInterface() && this.declaredMemberProperties.isEmpty() && this.declaredMemberFunctions.isEmpty()
