2f9b5ecd3d22c18e14a5d812c8de7be1e8abbcdd
Shane Myrick <mail@shanemyrick.com>
2018-12-13 12:07:44 -0800

    test: annotations on different types

--- src/test/kotlin/com/expedia/graphql/schema/extensions/KPropertyExtensionsKtTest.kt
+++ src/test/kotlin/com/expedia/graphql/schema/extensions/KPropertyExtensionsKtTest.kt
@@ -13,12 +13,18 @@ internal class KPropertyExtensionsKtTest {
     /**
      * Annotations can be on the property or on the contructor argument
      */
-    internal data class MyDataClass(
+    internal class MyClass(
         @property:Deprecated("property deprecated")
         @property:GraphQLDescription("property description")
         @property:GraphQLID
         @property:GraphQLIgnore
         val propertyAnnotation: String,
+
+        @property:Deprecated("property Int deprecated")
+        @property:GraphQLDescription("property Int description")
+        @property:GraphQLID
+        @property:GraphQLIgnore
+        val propertyAnnotationInt: Int,
 
         @Deprecated("constructor deprecated")
         @GraphQLDescription("constructor description")
@@ -31,29 +37,33 @@ internal class KPropertyExtensionsKtTest {
 
     @Test
     fun isPropertyGraphQLID() {
-        assertTrue(MyDataClass::propertyAnnotation.isPropertyGraphQLID(MyDataClass::class))
-        assertTrue(MyDataClass::constructorAnnotation.isPropertyGraphQLID(MyDataClass::class))
-        assertFalse(MyDataClass::noAnnotations.isPropertyGraphQLID(MyDataClass::class))
+        assertTrue(MyClass::propertyAnnotation.isPropertyGraphQLID(MyClass::class))
+        assertTrue(MyClass::propertyAnnotationInt.isPropertyGraphQLID(MyClass::class))
+        assertTrue(MyClass::constructorAnnotation.isPropertyGraphQLID(MyClass::class))
+        assertFalse(MyClass::noAnnotations.isPropertyGraphQLID(MyClass::class))
     }
 
     @Test
     fun isPropertyGraphQLIgnored() {
-        assertTrue(MyDataClass::propertyAnnotation.isPropertyGraphQLIgnored(MyDataClass::class))
-        assertTrue(MyDataClass::constructorAnnotation.isPropertyGraphQLIgnored(MyDataClass::class))
-        assertFalse(MyDataClass::noAnnotations.isPropertyGraphQLIgnored(MyDataClass::class))
+        assertTrue(MyClass::propertyAnnotation.isPropertyGraphQLIgnored(MyClass::class))
+        assertTrue(MyClass::propertyAnnotationInt.isPropertyGraphQLIgnored(MyClass::class))
+        assertTrue(MyClass::constructorAnnotation.isPropertyGraphQLIgnored(MyClass::class))
+        assertFalse(MyClass::noAnnotations.isPropertyGraphQLIgnored(MyClass::class))
     }
 
     @Test
     fun getPropertyDeprecationReason() {
-        assertEquals("property deprecated", MyDataClass::propertyAnnotation.getPropertyDeprecationReason(MyDataClass::class))
-        assertEquals("constructor deprecated", MyDataClass::constructorAnnotation.getPropertyDeprecationReason(MyDataClass::class))
-        assertEquals(null, MyDataClass::noAnnotations.getPropertyDeprecationReason(MyDataClass::class))
+        assertEquals("property deprecated", MyClass::propertyAnnotation.getPropertyDeprecationReason(MyClass::class))
+        assertEquals("property Int deprecated", MyClass::propertyAnnotationInt.getPropertyDeprecationReason(MyClass::class))
+        assertEquals("constructor deprecated", MyClass::constructorAnnotation.getPropertyDeprecationReason(MyClass::class))
+        assertEquals(null, MyClass::noAnnotations.getPropertyDeprecationReason(MyClass::class))
     }
 
     @Test
     fun getPropertyDescription() {
-        assertEquals("property description", MyDataClass::propertyAnnotation.getPropertyDescription(MyDataClass::class))
-        assertEquals("constructor description", MyDataClass::constructorAnnotation.getPropertyDescription(MyDataClass::class))
-        assertEquals(null, MyDataClass::noAnnotations.getPropertyDescription(MyDataClass::class))
+        assertEquals("property description", MyClass::propertyAnnotation.getPropertyDescription(MyClass::class))
+        assertEquals("property Int description", MyClass::propertyAnnotationInt.getPropertyDescription(MyClass::class))
+        assertEquals("constructor description", MyClass::constructorAnnotation.getPropertyDescription(MyClass::class))
+        assertEquals(null, MyClass::noAnnotations.getPropertyDescription(MyClass::class))
     }
 }
