87e47701c2989e0d412881de7df49aea6fa9eb39
Shane Myrick <mail@shanemyrick.com>
2018-12-13 12:21:19 -0800

    test: linter - use data class

--- src/test/kotlin/com/expedia/graphql/schema/extensions/KPropertyExtensionsKtTest.kt
+++ src/test/kotlin/com/expedia/graphql/schema/extensions/KPropertyExtensionsKtTest.kt
@@ -13,7 +13,7 @@ internal class KPropertyExtensionsKtTest {
     /**
      * Annotations can be on the property or on the contructor argument
      */
-    internal class MyClass(
+    internal data class MyDataClass(
         @property:Deprecated("property deprecated")
         @property:GraphQLDescription("property description")
         @property:GraphQLID
@@ -31,29 +31,29 @@ internal class KPropertyExtensionsKtTest {
 
     @Test
     fun isPropertyGraphQLID() {
-        assertTrue(MyClass::propertyAnnotation.isPropertyGraphQLID(MyClass::class))
-        assertTrue(MyClass::constructorAnnotation.isPropertyGraphQLID(MyClass::class))
-        assertFalse(MyClass::noAnnotations.isPropertyGraphQLID(MyClass::class))
+        assertTrue(MyDataClass::propertyAnnotation.isPropertyGraphQLID(MyDataClass::class))
+        assertTrue(MyDataClass::constructorAnnotation.isPropertyGraphQLID(MyDataClass::class))
+        assertFalse(MyDataClass::noAnnotations.isPropertyGraphQLID(MyDataClass::class))
     }
 
     @Test
     fun isPropertyGraphQLIgnored() {
-        assertTrue(MyClass::propertyAnnotation.isPropertyGraphQLIgnored(MyClass::class))
-        assertTrue(MyClass::constructorAnnotation.isPropertyGraphQLIgnored(MyClass::class))
-        assertFalse(MyClass::noAnnotations.isPropertyGraphQLIgnored(MyClass::class))
+        assertTrue(MyDataClass::propertyAnnotation.isPropertyGraphQLIgnored(MyDataClass::class))
+        assertTrue(MyDataClass::constructorAnnotation.isPropertyGraphQLIgnored(MyDataClass::class))
+        assertFalse(MyDataClass::noAnnotations.isPropertyGraphQLIgnored(MyDataClass::class))
     }
 
     @Test
     fun getPropertyDeprecationReason() {
-        assertEquals("property deprecated", MyClass::propertyAnnotation.getPropertyDeprecationReason(MyClass::class))
-        assertEquals("constructor deprecated", MyClass::constructorAnnotation.getPropertyDeprecationReason(MyClass::class))
-        assertEquals(null, MyClass::noAnnotations.getPropertyDeprecationReason(MyClass::class))
+        assertEquals("property deprecated", MyDataClass::propertyAnnotation.getPropertyDeprecationReason(MyDataClass::class))
+        assertEquals("constructor deprecated", MyDataClass::constructorAnnotation.getPropertyDeprecationReason(MyDataClass::class))
+        assertEquals(null, MyDataClass::noAnnotations.getPropertyDeprecationReason(MyDataClass::class))
     }
 
     @Test
     fun getPropertyDescription() {
-        assertEquals("property description", MyClass::propertyAnnotation.getPropertyDescription(MyClass::class))
-        assertEquals("constructor description", MyClass::constructorAnnotation.getPropertyDescription(MyClass::class))
-        assertEquals(null, MyClass::noAnnotations.getPropertyDescription(MyClass::class))
+        assertEquals("property description", MyDataClass::propertyAnnotation.getPropertyDescription(MyDataClass::class))
+        assertEquals("constructor description", MyDataClass::constructorAnnotation.getPropertyDescription(MyDataClass::class))
+        assertEquals(null, MyDataClass::noAnnotations.getPropertyDescription(MyDataClass::class))
     }
 }
