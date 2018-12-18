ee7c15f9894da4eb518125e27737487a3c11e6dc
Dariusz Kuc <dkuc@expedia.com>
2018-12-18 11:04:00 -0600

    keep custom directive names if they are specified
    We should only normalize the default (class name) directive names.

--- example/src/main/kotlin/com.expedia.graphql.sample/Application.kt
+++ example/src/main/kotlin/com.expedia.graphql.sample/Application.kt
@@ -67,7 +67,6 @@ class Application {
                 SchemaPrinter.Options.defaultOptions()
                         .includeScalarTypes(true)
                         .includeExtendedScalarTypes(true)
-                        .includeIntrospectionTypes(true)
                         .includeSchemaDefintion(true)
             ).print(schema)
         )
