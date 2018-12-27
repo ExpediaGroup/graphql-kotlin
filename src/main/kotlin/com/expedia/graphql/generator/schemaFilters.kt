42bdc42a7a14eecc04b6a3b836f40f55ba990bea
Shane Myrick <mail@shanemyrick.com>
2018-12-26 18:21:12 -0800

    test: update test names

--- src/main/kotlin/com/expedia/graphql/generator/schemaFilters.kt
+++ src/main/kotlin/com/expedia/graphql/generator/schemaFilters.kt
@@ -5,13 +5,11 @@ import com.expedia.graphql.generator.extensions.isPropertyGraphQLIgnored
 import com.expedia.graphql.generator.extensions.qualifiedName
 import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
 import kotlin.reflect.KProperty
 import kotlin.reflect.KVisibility
 
 private typealias CallableFilter = (KCallable<*>) -> Boolean
 private typealias PropertyFilter = (KProperty<*>, KClass<*>) -> Boolean
-private typealias FunctionFilter = (KFunction<*>) -> Boolean
 
 private val blacklistFunctions: List<String> = listOf("annotationType", "toString", "copy", "equals", "hashCode")
 private val blacklistTypes: List<String> = listOf("kotlin.reflect.KClass")
@@ -27,4 +25,4 @@ private val isComponentFunction: CallableFilter = { it.name.matches(componentFun
 private val isNotBlacklistedFunction: CallableFilter = { isBlacklistedFunction(it).not() && isComponentFunction(it).not() }
 
 internal val propertyFilters: List<PropertyFilter> = listOf(isPropertyPublic, isNotBlacklistedType, isPropertyNotGraphQLIgnored)
-internal val functionFilters: List<FunctionFilter> = listOf(isPublic, isNotGraphQLIgnored, isNotBlacklistedFunction)
+internal val functionFilters: List<CallableFilter> = listOf(isPublic, isNotGraphQLIgnored, isNotBlacklistedFunction)
--- src/test/kotlin/com/expedia/graphql/generator/types/DirectiveTypeBuilderTest.kt
+++ src/test/kotlin/com/expedia/graphql/generator/types/DirectiveTypeBuilderTest.kt
@@ -2,14 +2,11 @@ package com.expedia.graphql.generator.types
 
 import com.expedia.graphql.annotations.GraphQLDescription
 import com.expedia.graphql.annotations.GraphQLDirective
-import com.expedia.graphql.exceptions.GraphQLKotlinException
 import com.expedia.graphql.generator.SchemaGenerator
 import com.expedia.graphql.generator.extensions.isTrue
 import com.expedia.graphql.testSchemaConfig
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
-import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -19,17 +16,17 @@ internal class DirectiveTypeBuilderTest {
     internal annotation class SimpleDirective
 
     @GraphQLDirective
-    internal annotation class DiretiveWithString(val string: String)
+    internal annotation class DirectiveWithString(val string: String)
 
     internal enum class Type {
         ONE, TWO
     }
 
     @GraphQLDirective
-    internal annotation class DiretiveWithEnum(val type: Type)
+    internal annotation class DirectiveWithEnum(val type: Type)
 
     @GraphQLDirective
-    internal annotation class DiretiveWithClass(val kclass: KClass<*>)
+    internal annotation class DirectiveWithClass(val kclass: KClass<*>)
 
     internal class MyClass {
 
@@ -41,54 +38,45 @@ internal class DirectiveTypeBuilderTest {
         @SimpleDirective
         fun simpleDirective(string: String) = string
 
-        @DiretiveWithString(string = "foo")
+        @DirectiveWithString(string = "foo")
         fun directiveWithString(string: String) = string
 
-        @DiretiveWithEnum(type = Type.TWO)
+        @DirectiveWithEnum(type = Type.TWO)
         fun directiveWithEnum(string: String) = string
 
-        @DiretiveWithClass(kclass = Type::class)
+        @DirectiveWithClass(kclass = Type::class)
         fun directiveWithClass(string: String) = string
     }
-
-    private fun KClass<*>.getMemberFunction(name: String): KFunction<*> =
-        this.declaredMemberFunctions.find { it.name == name } ?: throw GraphQLKotlinException("No functino of name $name")
 
     private val basicGenerator = SchemaGenerator(emptyList(), emptyList(), testSchemaConfig)
 
     @Test
     fun `no annotation`() {
-        val function = MyClass::class.getMemberFunction("noAnnotation")
-        assertTrue(basicGenerator.directives(function).isEmpty().isTrue())
+        assertTrue(basicGenerator.directives(MyClass::noAnnotation).isEmpty().isTrue())
     }
 
     @Test
     fun `no directive`() {
-        val function = MyClass::class.getMemberFunction("noDirective")
-        assertTrue(basicGenerator.directives(function).isEmpty().isTrue())
+        assertTrue(basicGenerator.directives(MyClass::noDirective).isEmpty().isTrue())
     }
 
     @Test
     fun `has directive`() {
-        val function = MyClass::class.getMemberFunction("simpleDirective")
-        assertEquals(expected = 1, actual = basicGenerator.directives(function).size)
+        assertEquals(expected = 1, actual = basicGenerator.directives(MyClass::simpleDirective).size)
     }
 
     @Test
     fun `has directive with string`() {
-        val function = MyClass::class.getMemberFunction("directiveWithString")
-        assertEquals(expected = 1, actual = basicGenerator.directives(function).size)
+        assertEquals(expected = 1, actual = basicGenerator.directives(MyClass::directiveWithString).size)
     }
 
     @Test
     fun `has directive with enum`() {
-        val function = MyClass::class.getMemberFunction("directiveWithEnum")
-        assertEquals(expected = 1, actual = basicGenerator.directives(function).size)
+        assertEquals(expected = 1, actual = basicGenerator.directives(MyClass::directiveWithEnum).size)
     }
 
     @Test
     fun `has directive with class`() {
-        val function = MyClass::class.getMemberFunction("directiveWithClass")
-        assertEquals(expected = 1, actual = basicGenerator.directives(function).size)
+        assertEquals(expected = 1, actual = basicGenerator.directives(MyClass::directiveWithClass).size)
     }
 }
