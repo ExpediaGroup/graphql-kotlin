a65874510328090554d1ee60195e4d6f221f0ac5
Shane Myrick <mail@shanemyrick.com>
2018-12-28 16:43:35 -0800

    fix: label correct exception (#121)

--- src/main/kotlin/com/expedia/graphql/toSchema.kt
+++ src/main/kotlin/com/expedia/graphql/toSchema.kt
@@ -1,25 +1,29 @@
 package com.expedia.graphql
 
+import com.expedia.graphql.exceptions.GraphQLKotlinException
 import com.expedia.graphql.exceptions.InvalidSchemaException
 import com.expedia.graphql.generator.SchemaGenerator
-import graphql.GraphQLException
 import graphql.schema.GraphQLSchema
 
 /**
  * Entry point to generate a graphql schema using reflection on the passed objects.
  *
- * @param queries List of [TopLevelObject] objects to use for GraphQL queries
- * @param mutations List of [TopLevelObject] objects to use for GraphQL mutations
+ * @param queries List of [TopLevelObject] to use for GraphQL queries
+ * @param mutations List of [TopLevelObject] to use for GraphQL mutations
  * @param config Schema generation configuration
+ *
+ * @return GraphQLSchema from graphql-java
  */
-@Throws(GraphQLException::class)
+@Throws(GraphQLKotlinException::class)
 fun toSchema(
     queries: List<TopLevelObject>,
     mutations: List<TopLevelObject> = emptyList(),
     config: SchemaGeneratorConfig
 ): GraphQLSchema {
+
     if (queries.isEmpty()) {
         throw InvalidSchemaException()
     }
+
     return SchemaGenerator(queries, mutations, config).generate()
 }
