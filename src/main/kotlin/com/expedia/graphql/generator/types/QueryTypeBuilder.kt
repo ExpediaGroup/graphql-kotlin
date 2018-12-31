47cd74906ffaffc07a71ab19e0415071d7cc58c9
Shane Myrick <mail@shanemyrick.com>
2018-12-31 11:33:58 -0800

    refactor: create separate top level builders (#123)
    Move the query and mutation builder code to their own TypeBuilder class

---
+++ src/main/kotlin/com/expedia/graphql/generator/types/QueryTypeBuilder.kt
@@ -0,0 +1,33 @@
+package com.expedia.graphql.generator.types
+
+import com.expedia.graphql.TopLevelObject
+import com.expedia.graphql.exceptions.InvalidSchemaException
+import com.expedia.graphql.generator.SchemaGenerator
+import com.expedia.graphql.generator.TypeBuilder
+import com.expedia.graphql.generator.extensions.getValidFunctions
+import graphql.schema.GraphQLObjectType
+
+internal class QueryTypeBuilder(generator: SchemaGenerator) : TypeBuilder(generator) {
+
+    @Throws(InvalidSchemaException::class)
+    fun getQueryObject(queries: List<TopLevelObject>): GraphQLObjectType {
+
+        if (queries.isEmpty()) {
+            throw InvalidSchemaException()
+        }
+
+        val queryBuilder = GraphQLObjectType.Builder()
+        queryBuilder.name(config.topLevelQueryName)
+
+        for (query in queries) {
+            query.kClass.getValidFunctions(config.hooks)
+                .forEach {
+                    val function = generator.function(it, query.obj)
+                    val functionFromHook = config.hooks.didGenerateQueryType(it, function)
+                    queryBuilder.field(functionFromHook)
+                }
+        }
+
+        return queryBuilder.build()
+    }
+}
---
+++ src/test/kotlin/com/expedia/graphql/generator/types/MutationTypeBuilderTest.kt
@@ -0,0 +1,74 @@
+package com.expedia.graphql.generator.types
+
+import com.expedia.graphql.TopLevelObject
+import com.expedia.graphql.annotations.GraphQLDescription
+import com.expedia.graphql.annotations.GraphQLIgnore
+import com.expedia.graphql.generator.extensions.isTrue
+import com.expedia.graphql.hooks.SchemaGeneratorHooks
+import graphql.schema.GraphQLFieldDefinition
+import kotlin.reflect.KFunction
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+internal class MutationTypeBuilderTest : TypeTestHelper() {
+
+    internal class SimpleHooks : SchemaGeneratorHooks {
+        var calledHook = false
+        override fun didGenerateMutationType(function: KFunction<*>, fieldDefinition: GraphQLFieldDefinition): GraphQLFieldDefinition {
+            calledHook = true
+            return super.didGenerateMutationType(function, fieldDefinition)
+        }
+    }
+
+    internal class MutationObject {
+        @GraphQLDescription("A GraphQL mutation method")
+        fun mutation(value: Int) = value
+    }
+
+    internal class NoFunctions {
+        @GraphQLIgnore
+        fun hidden(value: Int) = value
+    }
+
+    private lateinit var builder: MutationTypeBuilder
+
+    override fun beforeSetup() {
+        hooks = SimpleHooks()
+    }
+
+    override fun beforeTest() {
+        builder = MutationTypeBuilder(generator)
+    }
+
+    @Test
+    fun `empty list`() {
+        assertNull(builder.getMutationObject(emptyList()))
+    }
+
+    @Test
+    fun `mutation with no valid functions`() {
+        val queries = listOf(TopLevelObject(NoFunctions()))
+        val result = builder.getMutationObject(queries)
+        assertEquals(expected = "TestTopLevelMutation", actual = result?.name)
+        assertTrue(result?.fieldDefinitions?.isEmpty().isTrue())
+    }
+
+    @Test
+    fun `mutation with valid functions`() {
+        val queries = listOf(TopLevelObject(MutationObject()))
+        val result = builder.getMutationObject(queries)
+        assertEquals(expected = "TestTopLevelMutation", actual = result?.name)
+        assertEquals(expected = 1, actual = result?.fieldDefinitions?.size)
+        assertEquals(expected = "mutation", actual = result?.fieldDefinitions?.firstOrNull()?.name)
+    }
+
+    @Test
+    fun `verify hooks are called`() {
+        assertFalse((hooks as? SimpleHooks)?.calledHook.isTrue())
+        builder.getMutationObject(listOf(TopLevelObject(MutationObject())))
+        assertTrue((hooks as? SimpleHooks)?.calledHook.isTrue())
+    }
+}
