ce018a9876c2de503b3847043992d71c70bf19cd
Shane Myrick <mail@shanemyrick.com>
2019-01-04 07:20:33 -0800

    fix: simplify directive wiring (#132)

--- src/main/kotlin/com/expedia/graphql/DirectiveWiringHelper.kt
+++ src/main/kotlin/com/expedia/graphql/DirectiveWiringHelper.kt
@@ -1,30 +1,26 @@
 package com.expedia.graphql
 
-import graphql.Assert.assertNotNull
-import graphql.schema.GraphQLArgument
+import com.expedia.graphql.exceptions.InvalidSchemaDirectiveWiringException
+import com.expedia.graphql.generator.extensions.getAllDirectives
+import com.expedia.graphql.generator.extensions.wireOnEnvironment
 import graphql.schema.GraphQLDirective
 import graphql.schema.GraphQLDirectiveContainer
-import graphql.schema.GraphQLEnumType
-import graphql.schema.GraphQLEnumValueDefinition
-import graphql.schema.GraphQLFieldDefinition
-import graphql.schema.GraphQLInputObjectField
-import graphql.schema.GraphQLInputObjectType
-import graphql.schema.GraphQLInterfaceType
-import graphql.schema.GraphQLObjectType
-import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
-import graphql.schema.GraphQLUnionType
 import graphql.schema.idl.SchemaDirectiveWiring
 import graphql.schema.idl.SchemaDirectiveWiringEnvironment
 import graphql.schema.idl.SchemaDirectiveWiringEnvironmentImpl
 import graphql.schema.idl.WiringFactory
 
 /**
- * Based on [SchemaGeneratorDirectiveHelper](https://github.com/graphql-java/graphql-java/blob/master/src/main/java/graphql/schema/idl/SchemaGeneratorDirectiveHelper.java) from graphql-java.
+ * Based on SchemaGeneratorDirectiveHelper from graphql-java.
  *
- * Relies on [WiringFactory] for providing applicable [SchemaDirectiveWiring] but if manual directive wiring map is provided it will take precedence over the wiring factory.
+ * Relies on [WiringFactory] for providing applicable [SchemaDirectiveWiring].
+ * If manual directive wiring map is provided it will take precedence over the wiring factory.
  */
-class DirectiveWiringHelper(private val wiringFactory: WiringFactory? = null, private val manualWiring: Map<String, SchemaDirectiveWiring> = mapOf()) {
+class DirectiveWiringHelper(
+    private val wiringFactory: WiringFactory? = null,
+    private val manualWiring: Map<String, SchemaDirectiveWiring> = mapOf()
+) {
 
     /**
      * Wire up the directive based on the GraphQL type
@@ -32,70 +28,31 @@ class DirectiveWiringHelper(private val wiringFactory: WiringFactory? = null, pr
     fun onWire(graphQLType: GraphQLType): GraphQLType {
         if (graphQLType !is GraphQLDirectiveContainer) return graphQLType
 
-        return wireDirectives(
-            graphQLType,
-            getDirectives(graphQLType),
-            ::createWiringEnvironment,
-            getInvoker(graphQLType)
-        )
+        return wireDirectives(graphQLType, graphQLType.getAllDirectives())
     }
 
-    private fun getDirectives(graphQLType: GraphQLDirectiveContainer): MutableList<GraphQLDirective> {
-        // A function without directives may still be rewired if the arguments have directives
-        // see https://github.com/ExpediaDotCom/graphql-kotlin/wiki/Schema-Directives for details
-        val directives = graphQLType.directives
-        if (graphQLType is GraphQLFieldDefinition) {
-            graphQLType.arguments.forEach { directives.addAll(it.directives) }
-        }
-        return directives
-    }
-
-    private fun getInvoker(graphQLType: GraphQLDirectiveContainer) =
-        { wiring: SchemaDirectiveWiring, environment: SchemaDirectiveWiringEnvironment<GraphQLDirectiveContainer> -> wireOnEnvironment(environment, wiring, graphQLType) }
-
-    private fun <T : GraphQLDirectiveContainer> createWiringEnvironment(element: T, directive: GraphQLDirective): SchemaDirectiveWiringEnvironment<T> =
-        // we are only specifying element and directive, other fields are not used by graphql-kotlin
-        SchemaDirectiveWiringEnvironmentImpl(element, directive, null, null, null)
-
-    private fun <T : GraphQLDirectiveContainer> wireDirectives(
-        element: T,
-        directives: List<GraphQLDirective>,
-        envBuilder: (T, GraphQLDirective) -> SchemaDirectiveWiringEnvironment<T>,
-        invoker: (SchemaDirectiveWiring, SchemaDirectiveWiringEnvironment<T>) -> T
-    ): T {
+    private fun wireDirectives(element: GraphQLDirectiveContainer, directives: List<GraphQLDirective>): GraphQLDirectiveContainer {
         var outputObject = element
+
         for (directive in directives) {
-            val env = envBuilder.invoke(outputObject, directive)
+            // we are only specifying element and directive, other fields are not used by graphql-kotlin
+            val env = SchemaDirectiveWiringEnvironmentImpl(outputObject, directive, null, null, null)
             val directiveWiring = discoverWiringProvider(directive.name, env)
             if (directiveWiring != null) {
-                val newElement = invoker.invoke(directiveWiring, env)
-                assertNotNull(newElement, "The SchemaDirectiveWiring MUST return a non null return value for element '" + element.name + "'")
+                val newElement = directiveWiring.wireOnEnvironment(env)
+                    ?: throw InvalidSchemaDirectiveWiringException(element.name)
+
                 outputObject = newElement
             }
         }
+
         return outputObject
     }
 
-    private fun <T : GraphQLDirectiveContainer> discoverWiringProvider(directiveName: String, env: SchemaDirectiveWiringEnvironment<T>): SchemaDirectiveWiring? =
+    private fun discoverWiringProvider(directiveName: String, env: SchemaDirectiveWiringEnvironment<GraphQLDirectiveContainer>): SchemaDirectiveWiring? =
         when {
             directiveName in manualWiring -> manualWiring[directiveName]
             true == wiringFactory?.providesSchemaDirectiveWiring(env) -> wiringFactory.getSchemaDirectiveWiring(env)
             else -> null
-        }
-
-    @Suppress("UNCHECKED_CAST", "Detekt.ComplexMethod")
-    private fun wireOnEnvironment(environment: SchemaDirectiveWiringEnvironment<GraphQLDirectiveContainer>, wiring: SchemaDirectiveWiring, generatedType: GraphQLDirectiveContainer) =
-        when (environment.element) {
-            is GraphQLObjectType -> wiring.onObject(environment as SchemaDirectiveWiringEnvironment<GraphQLObjectType>)
-            is GraphQLFieldDefinition -> wiring.onField(environment as SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition>)
-            is GraphQLInterfaceType -> wiring.onInterface(environment as SchemaDirectiveWiringEnvironment<GraphQLInterfaceType>)
-            is GraphQLUnionType -> wiring.onUnion(environment as SchemaDirectiveWiringEnvironment<GraphQLUnionType>)
-            is GraphQLScalarType -> wiring.onScalar(environment as SchemaDirectiveWiringEnvironment<GraphQLScalarType>)
-            is GraphQLEnumType -> wiring.onEnum(environment as SchemaDirectiveWiringEnvironment<GraphQLEnumType>)
-            is GraphQLEnumValueDefinition -> wiring.onEnumValue(environment as SchemaDirectiveWiringEnvironment<GraphQLEnumValueDefinition>)
-            is GraphQLArgument -> wiring.onArgument(environment as SchemaDirectiveWiringEnvironment<GraphQLArgument>)
-            is GraphQLInputObjectType -> wiring.onInputObjectType(environment as SchemaDirectiveWiringEnvironment<GraphQLInputObjectType>)
-            is GraphQLInputObjectField -> wiring.onInputObjectField(environment as SchemaDirectiveWiringEnvironment<GraphQLInputObjectField>)
-            else -> generatedType
         }
 }
