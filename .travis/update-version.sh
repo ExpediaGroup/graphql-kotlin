6e943e897fdf336d32c92b91bf3a33246b70e3b0
Travis CI <travis@travis-ci.org>
2019-01-08 10:18:14 -0800

    build: update script

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -2,19 +2,67 @@
 
 cd `dirname $0`/..
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+update_version() {
+
+    exampleProperty="graphql-kotlin.version"
+
+    # Push the new tag with `-SNAPSHOT` as the current version
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${TRAVIS_TAG}-SNAPSHOT"
+
+    # Increment the patch version
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set -DnextSnapshot=true
+
+    # Pull the value from the pom
+    NEW_VERSION=$(mvn --settings .travis/settings.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+    # Update the example version
+    cd example/
+    mvn --settings ../.travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set-property -Dproperty=${exampleProperty} -DnewVersion=${NEW_VERSION}
+    cd ../
+}
+
+commit_files() {
+
+  # Use the version as the branch name
+  git checkout -b ${NEW_VERSION}
+
+  # Stage the modified files
+  git add pom.xml example/pom.xml
+
+  # Create a new commit with a custom build message and Travis build number for reference
+  git commit -m "travis: Upgrade to next snapshot (Build $TRAVIS_BUILD_NUMBER)"
+}
+
+upload_files() {
+
+  tokenLink="https://${GITHUB_TOKEN}@github.com/ExpediaDotCom/graphql-kotlin.git"
+
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add token-origin ${tokenLink} > /dev/null 2>&1
+
+  # Push changes to the new branch
+  git push --quiet --set-upstream token-origin ${NEW_VERSION}
+
+  # Remove the origin to hide the token
+  git remote rm token-origin > /dev/null 2>&1
+}
+
 if [[ -z "$TRAVIS_TAG" ]]
 then
     echo "ERROR! Please set TRAVIS_TAG environment variable"
     exit 1
 fi
 
-if [[ -z "$GH_TOKEN" ]]
+if [[ -z "$GITHUB_TOKEN" ]]
 then
-    echo "ERROR! Please set GH_TOKEN environment variable"
+    echo "ERROR! Please set GITHUB_TOKEN environment variable"
     exit 1
 fi
-
-BRANCH_NAME=""
 
 NEW_VERSION=""
 
@@ -32,53 +80,5 @@ else
   echo "Nothing to do"
 fi
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
 
-update_version() {
-
-    exampleProperty="graphql-kotlin.version"
-
-    # Push the new as tag with `-SNAPSHOT`
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${TRAVIS_TAG}-SNAPSHOT"
-
-    # Increment the patch version
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set -DnextSnapshot=true
-
-    # Pull the number from the pom
-    NEW_VERSION=$(mvn --settings .travis/settings.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-    # Update the example version
-    cd example/
-    mvn --settings ../.travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set-property -Dproperty=${exampleProperty} -DnewVersion=${NEW_VERSION}
-}
-
-commit_files() {
-
-  BRANCH_NAME=${NEW_VERSION}
-
-  git checkout -b ${BRANCH_NAME}
-
-  # Stage the modified files
-  git add pom.xml example/pom.xml
-
-  # Create a new commit with a custom build message and Travis build number for reference
-  git commit -m "build: Upgrade to next snapshot (Build $TRAVIS_BUILD_NUMBER)"
-}
-
-upload_files() {
-
-  origin="https://${GH_TOKEN}@github.com/ExpediaDotCom/graphql-kotlin.git"
-
-  # Remove existing "origin"
-  git remote rm origin
-
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add origin ${origin} > /dev/null 2>&1
-
-  # Push changes to the new branch
-  git push --quiet --set-upstream origin ${BRANCH_NAME}
-}
 
