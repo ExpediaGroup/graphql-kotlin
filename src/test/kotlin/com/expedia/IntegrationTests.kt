30823c5a510153cc3e0e5defa7fb520fadd9c0bd
darken <darken@darken.eu>
2019-01-22 16:22:49 +0100

    An integration test for optional results.
    See #150

---
+++ src/test/kotlin/com/expedia/IntegrationTests.kt
@@ -0,0 +1,44 @@
+package com.expedia
+
+import com.expedia.graphql.TopLevelObject
+import com.expedia.graphql.testSchemaConfig
+import com.expedia.graphql.toSchema
+import graphql.GraphQL
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+@Suppress("Detekt.UnusedPrivateMember",
+    "Detekt.FunctionOnlyReturningConstant",
+    "Detekt.LargeClass",
+    "Detekt.MethodOverloading")
+class IntegrationTests {
+    @Test
+    fun `SchemaGenerator generates a simple GraphQL schema`() {
+        val schema = toSchema(
+            listOf(TopLevelObject(QueryObject())),
+            listOf(),
+            config = testSchemaConfig
+        )
+        val graphQL = GraphQL.newGraphQL(schema).build()
+
+        val result = graphQL.execute("{optionalResults{required optional}}")
+        val data: Map<String, Map<String, Any>>? = result.getData()
+
+        val optionalResults = data?.get("optionalResults")
+        assertNotNull(optionalResults)
+        assertEquals(2, optionalResults.size)
+        assertEquals("req", optionalResults["required"])
+        assertNull(optionalResults["optional"])
+    }
+
+    data class HasOptionalData(
+        val required: String,
+        val optional: String?
+    )
+
+    class QueryObject {
+        fun optionalResults() = HasOptionalData("req", null)
+    }
+}
