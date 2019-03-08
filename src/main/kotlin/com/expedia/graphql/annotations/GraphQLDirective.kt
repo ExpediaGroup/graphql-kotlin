eb6237c25e7c9a4d2a596e6cbb99e8e74dd3d60c
Brennan Taylor <brentaylor@expedia.com>
2019-03-08 09:02:54 -0800

    fix: default directive annotation to all locations in the schema

--- src/main/kotlin/com/expedia/graphql/annotations/GraphQLDirective.kt
+++ src/main/kotlin/com/expedia/graphql/annotations/GraphQLDirective.kt
@@ -1,11 +1,23 @@
 package com.expedia.graphql.annotations
 
 import graphql.introspection.Introspection.DirectiveLocation
+import graphql.introspection.Introspection.DirectiveLocation.ARGUMENT_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.ENUM
+import graphql.introspection.Introspection.DirectiveLocation.ENUM_VALUE
 import graphql.introspection.Introspection.DirectiveLocation.FIELD
 import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.FRAGMENT_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.FRAGMENT_SPREAD
+import graphql.introspection.Introspection.DirectiveLocation.INLINE_FRAGMENT
+import graphql.introspection.Introspection.DirectiveLocation.INPUT_FIELD_DEFINITION
+import graphql.introspection.Introspection.DirectiveLocation.INPUT_OBJECT
+import graphql.introspection.Introspection.DirectiveLocation.INTERFACE
 import graphql.introspection.Introspection.DirectiveLocation.MUTATION
 import graphql.introspection.Introspection.DirectiveLocation.OBJECT
 import graphql.introspection.Introspection.DirectiveLocation.QUERY
+import graphql.introspection.Introspection.DirectiveLocation.SCALAR
+import graphql.introspection.Introspection.DirectiveLocation.SCHEMA
+import graphql.introspection.Introspection.DirectiveLocation.UNION
 
 /**
  * Meta annotation used to denote an annotation as a GraphQL directive.
@@ -14,5 +26,23 @@ import graphql.introspection.Introspection.DirectiveLocation.QUERY
 annotation class GraphQLDirective(
     val name: String = "",
     val description: String = "",
-    val locations: Array<DirectiveLocation> = [QUERY, MUTATION, FIELD, FIELD_DEFINITION, OBJECT]
+    val locations: Array<DirectiveLocation> = [
+        QUERY,
+        MUTATION,
+        FIELD,
+        FRAGMENT_DEFINITION,
+        FRAGMENT_SPREAD,
+        INLINE_FRAGMENT,
+        SCHEMA,
+        SCALAR,
+        OBJECT,
+        FIELD_DEFINITION,
+        ARGUMENT_DEFINITION,
+        INTERFACE,
+        UNION,
+        ENUM,
+        ENUM_VALUE,
+        INPUT_OBJECT,
+        INPUT_FIELD_DEFINITION
+    ]
 )
