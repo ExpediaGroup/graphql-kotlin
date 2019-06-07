625dafae8c508a2d6e3fbbb9bc0ac9d5e2d9ea93
Shane Myrick <smyrick@expedia.com>
2019-06-07 14:52:01 -0700

    fix: update TypeNotSupportedException message (#236)
    Update the exception message to be clear about the situations in which the exception can be thrown. One of the most common questions we get is around trying to use ` Map<>` which is not a valid type for a GraphQL schema.

--- src/main/kotlin/com/expedia/graphql/exceptions/TypeNotSupportedException.kt
+++ src/main/kotlin/com/expedia/graphql/exceptions/TypeNotSupportedException.kt
@@ -6,4 +6,4 @@ import kotlin.reflect.KType
  * Thrown when the generator does not have a type to map to in GraphQL or in the hooks.
  */
 class TypeNotSupportedException(kType: KType, packageList: List<String>)
-    : GraphQLKotlinException("Cannot convert $kType since it is outside the supported packages $packageList")
+    : GraphQLKotlinException("Cannot convert $kType since it is not a valid GraphQL type or outside the supported packages $packageList")
