a813d3f47c647c5794a0be3404ffe50ade912db0
Shane Myrick <mail@shanemyrick.com>
2019-07-25 12:19:10 -0700

    Add debug logging to update-versions.sh (#279)
    * Add debug logging to update-versions.sh
    
    Add some more logging to the script because running the steps locally performs the correct changes
    
    * print git status info

--- .travis/update-version.sh
+++ .travis/update-version.sh
@@ -1,47 +1,53 @@
 #!/usr/bin/env bash
 
-cd `dirname $0`/..
-
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+    echo "Setting up git config"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
 }
 
 update_version() {
-    # Push the new tag with `-SNAPSHOT` as the current version
+    echo "Updating the version in the POMs"
+
+    # Push the tag with `-SNAPSHOT` as the current version
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.7:set -DnewVersion="${TRAVIS_TAG}-SNAPSHOT"
 
     # Increment the patch version
     mvn --settings .travis/settings.xml release:update-versions -B
 
-    # Pull the value from the pom
+    # Pull the new value from the pom
     NEW_VERSION=$(mvn --settings .travis/settings.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 }
 
 commit_files() {
+    echo "Commiting the changes to git"
 
-  # Use the version as the branch name
-  git checkout -b ${NEW_VERSION}
+    # Use the version as the branch name
+    git checkout -b ${NEW_VERSION}
 
-  # Stage the modified files
-  git add pom.xml */pom.xml
+    # Stage the modified files
+    git add ./pom.xml ./*/pom.xml
 
-  # Create a new commit with a custom build message and Travis build number for reference
-  git commit -m "build: $NEW_VERSION (Travis Build $TRAVIS_BUILD_NUMBER)"
+    # Print the current files we are going to commit
+    git status
+
+    # Create a new commit with a custom build message and Travis build number for reference
+    git commit -m "build: $NEW_VERSION (Travis Build $TRAVIS_BUILD_NUMBER)"
 }
 
 upload_files() {
+    echo "Pushing the changes to git remote"
 
-  tokenLink="https://${GITHUB_TOKEN}@github.com/ExpediaDotCom/graphql-kotlin.git"
+    tokenLink="https://${GITHUB_TOKEN}@github.com/ExpediaDotCom/graphql-kotlin.git"
 
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add token-origin ${tokenLink} > /dev/null 2>&1
+    # Add new "origin" with access token in the git URL for authentication
+    git remote add token-origin ${tokenLink} > /dev/null 2>&1
 
-  # Push changes to the new branch
-  git push --quiet --set-upstream token-origin ${NEW_VERSION}
+    # Push changes to the new branch
+    git push --quiet --set-upstream token-origin ${NEW_VERSION}
 
-  # Remove the origin to hide the token
-  git remote rm token-origin > /dev/null 2>&1
+    # Remove the origin to hide the token
+    git remote rm token-origin > /dev/null 2>&1
 }
 
 if [[ -z "$TRAVIS_TAG" ]]
@@ -58,6 +64,10 @@ fi
 
 NEW_VERSION=""
 
+cd `dirname $0`/..
+
+pwd
+
 setup_git
 
 update_version
@@ -66,11 +76,7 @@ commit_files
 
 # Attempt to push to git only if "git commit" succeeded
 if [[ $? -eq 0 ]]; then
-  echo "Uploading to GitHub"
   upload_files
 else
-  echo "Nothing to do"
+  echo "No files commited. Not pushing anything to git."
 fi
-
-
-
