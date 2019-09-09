df0e482f5ac7835c466656df34654899f94b8f40
smyrick <smyrick@expedia.com>
2019-09-09 10:49:31 -0700

    Update copyright name

--- graphql-kotlin-schema-generator/src/main/kotlin/com/expediagroup/graphql/exceptions/InvalidQueryTypeException.kt
+++ graphql-kotlin-schema-generator/src/main/kotlin/com/expediagroup/graphql/exceptions/InvalidQueryTypeException.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Expedia Group
+ * Copyright 2019 Expedia Group, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
