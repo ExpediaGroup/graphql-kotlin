90b607fb7ca08bfa359aae85a005ba9f9dad14c3
smyrick <smyrick@expedia.com>
2019-09-25 15:38:58 -0700

    Rename examples package
    Rename the package from com.expediagroup.graphql.sample to com.expediagroup.graphql.examples to match directory name

---
+++ examples/spring/src/main/kotlin/com/expediagroup/graphql/examples/query/AsyncQuery.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Expedia, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.expediagroup.graphql.examples.query
+
+import com.expediagroup.graphql.annotations.GraphQLDescription
+import com.expediagroup.graphql.spring.operations.Query
+import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
+
+/**
+ * Example async queries.
+ */
+@Component
+class AsyncQuery : Query {
+
+    @GraphQLDescription("Delays for given amount and then echos the string back."
+            + " The default async executor will work with CompletableFuture."
+            + " To use other rx frameworks you'll need to install a custom one to handle the types correctly.")
+    fun delayedEchoUsingCompletableFuture(msg: String, delaySeconds: Int): CompletableFuture<String> {
+        val future = CompletableFuture<String>()
+        Thread {
+            Thread.sleep(delaySeconds * 1000L)
+            future.complete(msg)
+        }.start()
+        return future
+    }
+}
---
+++ examples/spring/src/main/kotlin/com/expediagroup/graphql/examples/query/SubQueries.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Expedia, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.expediagroup.graphql.examples.query
+
+import com.expediagroup.graphql.annotations.GraphQLDescription
+import com.expediagroup.graphql.annotations.GraphQLIgnore
+import com.expediagroup.graphql.spring.operations.Query
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.BeanFactoryAware
+import org.springframework.beans.factory.getBean
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+@Component
+class SubQueries: Query, BeanFactoryAware {
+    private lateinit var beanFactory: BeanFactory
+
+    @GraphQLIgnore
+    override fun setBeanFactory(beanFactory: BeanFactory) {
+        this.beanFactory = beanFactory
+    }
+
+    @GraphQLDescription(
+        """Creates a hierarchy of executable fields.
+        Arguments can be passed from `main` to the `SubQuery` via the constructor invoke by the bean factory"""
+    )
+    fun main(): SubQuery = beanFactory.getBean()
+}
+
+@Component
+@Scope("prototype")
+class SubQuery {
+    fun secondary() = "secondary"
+}
