a844a122a13b7faf0031f9ce0ea41b013fb51eab
Shane Myrick <mail@shanemyrick.com>
2019-10-01 12:30:35 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'github',
   },
 
   // Add custom scripts here that would be placed in <script> tags.
