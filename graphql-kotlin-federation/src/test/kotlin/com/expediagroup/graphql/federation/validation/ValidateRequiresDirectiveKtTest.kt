db162cd9c19ba83265e6b69eb9e0aa6c14cf965c
Shane Myrick <accounts@shanemyrick.com>
2020-01-10 14:05:28 -0800

    Update requires directive tests

--- graphql-kotlin-federation/src/test/kotlin/com/expediagroup/graphql/federation/validation/ValidateRequiresDirectiveKtTest.kt
+++ graphql-kotlin-federation/src/test/kotlin/com/expediagroup/graphql/federation/validation/ValidateRequiresDirectiveKtTest.kt
@@ -29,20 +29,14 @@ import kotlin.test.assertEquals
 
 class ValidateRequiresDirectiveKtTest {
 
-    private val requiresDirective: GraphQLDirective = mockk {
+    private fun requiresDirective(directiveValue: String = "weight"): GraphQLDirective = mockk {
         every { name } returns "requires"
         every { getArgument(eq("fields")) } returns mockk {
             every { value } returns mockk<FieldSet> {
-                every { value } returns "weight"
+                every { value } returns directiveValue
             }
         }
     }
-
-    private val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
-        .name("shippingCost")
-        .type(GraphQLString)
-        .withDirective(requiresDirective)
-        .build()
 
     private val weight = GraphQLFieldDefinition.newFieldDefinition()
         .name("weight")
@@ -51,20 +45,41 @@ class ValidateRequiresDirectiveKtTest {
 
     /**
      * type Foo {
+     *   shippingCost: String
+     * }
+     */
+    @Test
+    fun `Verify non extended types and non requries fields returns an error`() {
+        val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
+            .name("shippingCost")
+            .type(GraphQLString)
+            .build()
+
+        val errors = validateRequiresDirective(
+            validatedType = "Foo",
+            fieldMap = emptyMap(),
+            validatedField = shippingCost,
+            extendedType = false)
+
+        assertEquals(1, errors.size)
+        assertEquals("base Foo type has fields marked with @requires directive, validatedField=shippingCost", errors.first())
+    }
+
+    /**
+     * type Foo {
      *   shippingCost: String @requires(fields: "weight")
-     *   weight: Float
      * }
      */
     @Test
     fun `Verify non extended types return an error`() {
-        val validatedType = GraphQLObjectType.newObject()
-            .name("Foo")
-            .field(shippingCost)
-            .field(weight)
+        val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
+            .name("shippingCost")
+            .type(GraphQLString)
+            .withDirective(requiresDirective())
             .build()
 
         val errors = validateRequiresDirective(
-            validatedType = validatedType.name,
+            validatedType = "Foo",
             fieldMap = emptyMap(),
             validatedField = shippingCost,
             extendedType = false)
@@ -75,12 +90,52 @@ class ValidateRequiresDirectiveKtTest {
 
     /**
      * type Foo @extends {
+     *   shippingCost: String @requires(fields: "bar")
+     *   weight: Float
+     * }
+     */
+    @Test
+    fun `Verify valid requires directive, but invalid field set selection`() {
+        val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
+            .name("shippingCost")
+            .type(GraphQLString)
+            .withDirective(requiresDirective("bar"))
+            .build()
+
+        val validatedType = GraphQLObjectType.newObject()
+            .name("Foo")
+            .field(shippingCost)
+            .field(weight)
+            .build()
+
+        val errors = validateRequiresDirective(
+            validatedType = validatedType.name,
+            fieldMap = validatedType.fieldDefinitions.map { it.name to it }.toMap(),
+            validatedField = shippingCost,
+            extendedType = true)
+
+        assertEquals(1, errors.size)
+        assertEquals("@requires(fields = bar) directive on Foo.shippingCost specifies invalid field set - field set specifies fields that do not exist", errors.first())
+    }
+
+    /**
+     * type Foo @extends {
      *   shippingCost: String @requires(fields: "weight")
      *   weight: Float
      * }
      */
     @Test
-    fun `Verify valid requires directive, but invalid field selection`() {
+    fun `Verify valid requires directive and but selected field is not external`() {
+        val id = GraphQLFieldDefinition.newFieldDefinition()
+            .name("id")
+            .type(GraphQLString)
+
+        val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
+            .name("shippingCost")
+            .type(GraphQLString)
+            .withDirective(requiresDirective())
+            .build()
+
         val validatedType = GraphQLObjectType.newObject()
             .name("Foo")
             .field(shippingCost)
@@ -105,6 +160,16 @@ class ValidateRequiresDirectiveKtTest {
      */
     @Test
     fun `Verify valid requires directive and valid field selection`() {
+        val id = GraphQLFieldDefinition.newFieldDefinition()
+            .name("id")
+            .type(GraphQLString)
+
+        val shippingCost = GraphQLFieldDefinition.newFieldDefinition()
+            .name("shippingCost")
+            .type(GraphQLString)
+            .withDirective(requiresDirective())
+            .build()
+
         val modifiedWeight = GraphQLFieldDefinition.newFieldDefinition(weight)
             .withDirective(GraphQLDirective.newDirective().name("external"))
 
