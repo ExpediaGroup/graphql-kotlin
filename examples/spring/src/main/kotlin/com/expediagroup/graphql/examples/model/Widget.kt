a049856a5f757cf253eff7b5ae0c834d4bcc7963
Shane Myrick <accounts@shanemyrick.com>
2020-07-17 11:13:13 -0700

    Support renaming of enum values:

--- examples/spring/src/main/kotlin/com/expediagroup/graphql/examples/model/Widget.kt
+++ examples/spring/src/main/kotlin/com/expediagroup/graphql/examples/model/Widget.kt
@@ -21,13 +21,17 @@ import com.expediagroup.graphql.annotations.GraphQLIgnore
 
 @GraphQLDescription("A useful widget")
 data class Widget(
+
     @GraphQLDescription("The widget's value that can be null")
     var value: Int? = null,
+
     @Deprecated(message = "This field is deprecated", replaceWith = ReplaceWith("value"))
     @GraphQLDescription("The widget's deprecated value that shouldn't be used")
     val deprecatedValue: Int? = value,
+
     @GraphQLIgnore
     val ignoredField: String? = "ignored",
+
     private val hiddenField: String? = "hidden"
 ) {
 
