42f1955303d7c497216318109a588466ba242d73
Dariusz Kuc <dkuc@expedia.com>
2020-07-23 13:55:01 -0500

    [docs] cut documentation version for 3.x.x releases
    As we prepare to start working on v4.x.x features on master we are cutting current documentation version

---
+++ website/pages/en/versions.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary');
+
+const Container = CompLibrary.Container;
+
+const CWD = process.cwd();
+
+const versions = require(`${CWD}/versions.json`);
+
+function Versions(props) {
+  const {config: siteConfig} = props;
+  const latestVersion = versions[0];
+  const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+  return (
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer versionsContainer">
+        <div className="post">
+          <header className="postHeader">
+            <h1>{siteConfig.title} Versions</h1>
+          </header>
+          <h3 id="latest">Current version (Stable)</h3>
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>{latestVersion}</th>
+                <td>
+                  <a
+                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/getting-started.html`}>
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={`${repoUrl}/releases`}>Release Notes</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h3 id="rc">Pre-release versions</h3>
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>master</th>
+                <td>
+                  {/* You are supposed to change this href where appropriate
+                        Example: href="<baseUrl>/docs(/:language)/next/:id" */}
+                  <a
+                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/next/getting-started.html`}>
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={repoUrl}>Source Code</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          {/* TODO uncomment below once we release 4.x.x
+          <h3 id="archive">Past Versions</h3>
+          <p>Here you can find previous versions of the documentation.</p>
+          <table className="versions">
+            <tbody>
+              {versions.map(
+                version =>
+                  version !== latestVersion && (
+                    <tr key={version}>
+                      <th>{version}</th>
+                      <td>
+                        <a
+                          href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
+                            props.language ? props.language + '/' : ''
+                          }${version}/doc1`}>
+                          Documentation
+                        </a>
+                      </td>
+                      <td>
+                        <a href={`${repoUrl}/releases/tag/v${version}`}>
+                          Release Notes
+                        </a>
+                      </td>
+                    </tr>
+                  ),
+              )}
+            </tbody>
+          </table>
+          <p>
+            You can find past versions of this project on{' '}
+            <a href={repoUrl}>GitHub</a>.
+          </p>
+          */}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+module.exports = Versions;
