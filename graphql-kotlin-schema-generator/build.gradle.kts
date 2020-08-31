c81e866298d018c05d16e72192d59e9f4e111b47
Shane Myrick <accounts@shanemyrick.com>
2020-08-31 11:10:56 -0700

    Revert all changes except gradle update

--- graphql-kotlin-schema-generator/build.gradle.kts
+++ graphql-kotlin-schema-generator/build.gradle.kts
@@ -3,6 +3,7 @@ description = "Code-only GraphQL schema generation for Kotlin"
 val classGraphVersion: String by project
 val graphQLJavaVersion: String by project
 val jacksonVersion: String by project
+val kotlinVersion: String by project
 val kotlinCoroutinesVersion: String by project
 val rxjavaVersion: String by project
 val junitVersion: String by project
@@ -11,6 +12,7 @@ dependencies {
     api("com.graphql-java:graphql-java:$graphQLJavaVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$kotlinCoroutinesVersion")
     api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation(kotlin("reflect", kotlinVersion))
     implementation("io.github.classgraph:classgraph:$classGraphVersion")
     testImplementation("io.reactivex.rxjava3:rxjava:$rxjavaVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
