26aabd9c9b0eeeda66cdd6759f1878c800090367
Shane Myrick <mail@shanemyrick.com>
2020-08-31 11:37:08 -0700

    Include reflection as a dependency for all subprojects (#856)
    Co-authored-by: Shane Myrick <accounts@shanemyrick.com>

--- graphql-kotlin-schema-generator/build.gradle.kts
+++ graphql-kotlin-schema-generator/build.gradle.kts
@@ -3,7 +3,6 @@ description = "Code-only GraphQL schema generation for Kotlin"
 val classGraphVersion: String by project
 val graphQLJavaVersion: String by project
 val jacksonVersion: String by project
-val kotlinVersion: String by project
 val kotlinCoroutinesVersion: String by project
 val rxjavaVersion: String by project
 val junitVersion: String by project
@@ -12,7 +11,6 @@ dependencies {
     api("com.graphql-java:graphql-java:$graphQLJavaVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$kotlinCoroutinesVersion")
     api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation(kotlin("reflect", kotlinVersion))
     implementation("io.github.classgraph:classgraph:$classGraphVersion")
     testImplementation("io.reactivex.rxjava3:rxjava:$rxjavaVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
