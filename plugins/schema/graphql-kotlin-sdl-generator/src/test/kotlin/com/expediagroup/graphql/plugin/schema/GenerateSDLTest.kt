e575ec8551b123627e078bd818955de92c90279f
Dariusz Kuc <dkuc@expedia.com>
2021-01-07 20:48:53 -0600

    [plugins] create sdl-generator module that can generate graphql schema
    `graphql-kotlin-sdl-generator` module can be used to generate GraphQL schema build artifact directly from your source code. Module scans the classpath for classes implementing `graphql-kotlin-types` marker interfaces (`Query`, `Mutation` and `Subscription`) and then uses that information to generate a schema using `graphql-kotlin-schema-generator`. By default, this module will generate the schema using `NoopSchemaGeneratorHooks`. If your project uses custom hooks or needs to generate the federated GraphQL schema, you will need to provide an instance of `SchemaGeneratorHooksProvider` that will be used to create an instance of your custom hooks.
    
    `SchemaGeneratorHooksProvider` is a service provider interface that exposes a single `hooks` method to generate an instance of `SchemaGeneratorHooks` that will be used to generate your schema. By utilizing Java [ServiceLoader](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ServiceLoader.html) we can dynamically load custom providers directly from the classpath. Service provider can be provided as part of target project sources, included inside of one of the project dependencies or through explicitly provided artifact. Since we need to be able to deterministically choose a single hooks provider, generation of schema will fail if there are multiple providers on the classpath.

---
+++ plugins/schema/graphql-kotlin-sdl-generator/src/test/kotlin/com/expediagroup/graphql/plugin/schema/GenerateSDLTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 Expedia, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.expediagroup.graphql.plugin.schema
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class GenerateSDLTest {
+
+    @Test
+    fun `verify we can generate SDL using default hooks provider`() {
+        val expectedSchema =
+            """
+                schema {
+                  query: Query
+                }
+
+                "Directs the executor to include this field or fragment only when the `if` argument is true"
+                directive @include(
+                    "Included when true."
+                    if: Boolean!
+                  ) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+
+                "Directs the executor to skip this field or fragment when the `if`'argument is true."
+                directive @skip(
+                    "Skipped when true."
+                    if: Boolean!
+                  ) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+
+                "Marks the field or enum value as deprecated"
+                directive @deprecated(
+                    "The reason for the deprecation"
+                    reason: String = "No longer supported"
+                  ) on FIELD_DEFINITION | ENUM_VALUE
+
+                "Exposes a URL that specifies the behaviour of this scalar."
+                directive @specifiedBy(
+                    "The URL that specifies the behaviour of this scalar."
+                    url: String!
+                  ) on SCALAR
+
+                type Query {
+                  helloWorld(name: String): String!
+                }
+            """.trimIndent()
+        val generatedSchema = generateSDL(listOf("com.expediagroup.test"))
+
+        assertEquals(expectedSchema, generatedSchema.trim())
+    }
+
+    @Test
+    fun `verify that generation of SDL will fail if no queries are found`() {
+        val exception = assertThrows<RuntimeException> {
+            generateSDL(listOf("com.expediagroup.graphql.plugin.schema"))
+        }
+        assertEquals("Invalid query object type - no valid queries are available.", exception.message)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,13 +29,21 @@ rootProject.name = "graphql-kotlin"
 include(":graphql-kotlin-types")
 include(":graphql-kotlin-schema-generator")
 include(":graphql-kotlin-federation")
+
+// servers
 include(":graphql-kotlin-spring-server")
+
+// clients
 include(":graphql-kotlin-client")
 include(":graphql-kotlin-ktor-client")
 include(":graphql-kotlin-spring-client")
+
+// plugins
 include(":graphql-kotlin-plugin-core")
 include(":graphql-kotlin-gradle-plugin")
 include(":graphql-kotlin-maven-plugin")
+include(":graphql-kotlin-sdl-generator")
+include(":graphql-kotlin-federated-hooks-provider")
 
 project(":graphql-kotlin-client").projectDir = file("clients/graphql-kotlin-client")
 project(":graphql-kotlin-ktor-client").projectDir = file("clients/graphql-kotlin-ktor-client")
@@ -44,3 +52,5 @@ project(":graphql-kotlin-spring-client").projectDir = file("clients/graphql-kotl
 project(":graphql-kotlin-plugin-core").projectDir = file("plugins/graphql-kotlin-plugin-core")
 project(":graphql-kotlin-gradle-plugin").projectDir = file("plugins/graphql-kotlin-gradle-plugin")
 project(":graphql-kotlin-maven-plugin").projectDir = file("plugins/graphql-kotlin-maven-plugin")
+project(":graphql-kotlin-sdl-generator").projectDir = file("plugins/schema/graphql-kotlin-sdl-generator")
+project(":graphql-kotlin-federated-hooks-provider").projectDir = file("plugins/schema/graphql-kotlin-federated-hooks-provider")
