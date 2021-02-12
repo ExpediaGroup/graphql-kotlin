5b9debc197ebd6d29192f4f3827c78839f30003b
Shane Myrick <accounts@shanemyrick.com>
2021-02-11 16:11:54 -0800

    Remove shadow plugin

--- generator/graphql-kotlin-federation/build.gradle.kts
+++ generator/graphql-kotlin-federation/build.gradle.kts
@@ -3,10 +3,6 @@ description = "Federated GraphQL schema generator"
 val junitVersion: String by project
 val federationGraphQLVersion: String by project
 
-plugins {
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-}
-
 dependencies {
     api(project(path = ":graphql-kotlin-schema-generator"))
     api("com.apollographql.federation:federation-graphql-java-support:$federationGraphQLVersion")
@@ -14,13 +10,6 @@ dependencies {
 }
 
 tasks {
-    // Relocate the dependency so that users don't have to require jcenter
-    shadowJar {
-        minimize()
-        archiveClassifier.set("")
-        configurations = mutableListOf(project.configurations.compileClasspath.get())
-        relocate("com.apollographql.federation", "shadow.com.apollographql.federation")
-    }
 
     jacocoTestCoverageVerification {
         violationRules {
