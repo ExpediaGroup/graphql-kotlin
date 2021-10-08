8470ce26f650a47cfae7a54d398d4a26bd207708
Shane Myrick <smyrick@expediagroup.com>
2021-10-08 14:50:42 -0700

    Update other hardcoded versions

--- plugins/graphql-kotlin-gradle-plugin/src/integration/android/androidGraphQLClient/app/build.gradle.kts
+++ plugins/graphql-kotlin-gradle-plugin/src/integration/android/androidGraphQLClient/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 }
 
 val graphQLKotlinVersion = System.getenv("GRAPHQL_KOTLIN_VERSION") ?: "5.0.0-SNAPSHOT"
-val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.5.21"
+val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.5.31"
 dependencies {
     implementation("com.expediagroup:graphql-kotlin-ktor-client:$graphQLKotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
