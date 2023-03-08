80e241b9456fc69210e1d15a705619fd77011d51
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-03-08 16:06:08 -0600

    fix

--- integration/graphql-http-spec/http-spec.test.js
+++ integration/graphql-http-spec/http-spec.test.js
@@ -2,30 +2,7 @@ let { fetch } = require('@whatwg-node/fetch')
 let { serverAudits } = require('graphql-http')
 
 const targetUrl = 'http://localhost:8080/graphql';
-// verify server is up
-let attempts = 10;
-while (attempts--) {
-  try {
-    const healthCheck = await fetch(targetUrl, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query: 'query { __typename }',
-      })
-    })
 
-    if (healthCheck.data?.__typename) {
-        break;
-    }
-  } catch (e) {
-    // continue
-  }
-}
-
-// compliance tests
 for (const audit of serverAudits({
   url: targetUrl,
   fetchFn: fetch,
