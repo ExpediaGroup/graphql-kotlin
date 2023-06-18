4e363197d3bd842ea5c2db490d90f9851337a6b8
Samuel Vazquez <sam_2f@hotmail.com>
2023-06-18 11:32:07 -0700

    feat: rename const

--- executions/graphql-kotlin-dataloader-instrumentation/src/test/kotlin/com/expediagroup/graphql/dataloader/instrumentation/level/DataLoaderLevelDispatchedInstrumentationTest.kt
+++ executions/graphql-kotlin-dataloader-instrumentation/src/test/kotlin/com/expediagroup/graphql/dataloader/instrumentation/level/DataLoaderLevelDispatchedInstrumentationTest.kt
@@ -63,7 +63,7 @@ class DataLoaderLevelDispatchedInstrumentationTest {
         assertEquals(1, missionStatistics?.batchInvokeCount)
         assertEquals(2, missionStatistics?.batchLoadCount)
 
-        verify(exactly = 2 + PLUS_1_LEVEL) {
+        verify(exactly = 2 + PLUS_ONE_LEVEL) {
             kotlinDataLoaderRegistry.dispatchAll()
         }
     }
@@ -94,7 +94,7 @@ class DataLoaderLevelDispatchedInstrumentationTest {
         assertEquals(1, missionStatistics?.batchInvokeCount)
         assertEquals(2, missionStatistics?.batchLoadCount)
 
-        verify(exactly = 3 + PLUS_1_LEVEL) {
+        verify(exactly = 3 + PLUS_ONE_LEVEL) {
             kotlinDataLoaderRegistry.dispatchAll()
         }
     }
@@ -136,7 +136,7 @@ class DataLoaderLevelDispatchedInstrumentationTest {
         assertEquals(2, missionsByAstronautStatistics?.batchInvokeCount)
         assertEquals(2, missionsByAstronautStatistics?.batchLoadCount)
 
-        verify(exactly = 4 + PLUS_1_LEVEL) {
+        verify(exactly = 4 + PLUS_ONE_LEVEL) {
             kotlinDataLoaderRegistry.dispatchAll()
         }
     }
@@ -160,6 +160,6 @@ class DataLoaderLevelDispatchedInstrumentationTest {
     }
 
     companion object {
-        private const val PLUS_1_LEVEL = 1
+        private const val PLUS_ONE_LEVEL = 1
     }
 }
