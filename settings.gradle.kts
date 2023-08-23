2780128ed42908b88dc9892ee4662d78c8bc4079
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-08-23 17:03:35 -0500

    enable build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "graphql-kotlin"
 
+plugins {
+    id("com.gradle.enterprise") version("3.14.1")
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 // Clients
@@ -65,3 +69,10 @@ project(":graphql-kotlin-ktor-server").projectDir = file("servers/graphql-kotlin
 project(":graphql-kotlin-dataloader").projectDir = file("executions/graphql-kotlin-dataloader")
 project(":graphql-kotlin-dataloader-instrumentation").projectDir = file("executions/graphql-kotlin-dataloader-instrumentation")
 project(":graphql-kotlin-automatic-persisted-queries").projectDir = file("executions/graphql-kotlin-automatic-persisted-queries")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
