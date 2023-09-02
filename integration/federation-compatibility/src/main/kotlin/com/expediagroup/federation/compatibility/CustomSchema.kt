2dc5b0551d563babfbdb3d4472f5d24f8bc44ee6
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-09-01 20:14:43 -0500

    fix compilation integration test

--- integration/federation-compatibility/src/main/kotlin/com/expediagroup/federation/compatibility/CustomSchema.kt
+++ integration/federation-compatibility/src/main/kotlin/com/expediagroup/federation/compatibility/CustomSchema.kt
@@ -3,14 +3,17 @@ package com.expediagroup.federation.compatibility
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
 import com.expediagroup.graphql.generator.federation.directives.ComposeDirective
 import com.expediagroup.graphql.generator.federation.directives.LinkDirective
+import com.expediagroup.graphql.generator.federation.directives.LinkImport
+import com.expediagroup.graphql.generator.federation.directives.LinkedSpec
 import com.expediagroup.graphql.server.Schema
 import graphql.introspection.Introspection
 import org.springframework.stereotype.Component
 
-@LinkDirective(url = "https://myspecs.dev/myCustomDirective/v1.0", import = ["@custom"])
+@LinkDirective(url = "https://myspecs.dev/mySpec/v1.0", `as` = "mySpec", import = [LinkImport("@custom")])
 @ComposeDirective("@custom")
 @Component
 class CustomSchema : Schema
 
+@LinkedSpec("mySpec")
 @GraphQLDirective(name = "custom", locations = [Introspection.DirectiveLocation.OBJECT])
 annotation class CustomDirective
