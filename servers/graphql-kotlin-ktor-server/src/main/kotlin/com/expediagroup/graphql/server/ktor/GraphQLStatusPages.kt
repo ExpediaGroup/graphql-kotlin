cc5db1e630920419872baa4414176f0999ed1312
Curtis Cook <curtiscook@users.noreply.github.com>
2024-04-09 18:44:34 -0700

    Update servers/graphql-kotlin-ktor-server/src/main/kotlin/com/expediagroup/graphql/server/ktor/GraphQLStatusPages.kt
    Co-authored-by: Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>

--- servers/graphql-kotlin-ktor-server/src/main/kotlin/com/expediagroup/graphql/server/ktor/GraphQLStatusPages.kt
+++ servers/graphql-kotlin-ktor-server/src/main/kotlin/com/expediagroup/graphql/server/ktor/GraphQLStatusPages.kt
@@ -4,6 +4,13 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.plugins.statuspages.StatusPagesConfig
 import io.ktor.server.response.respond
 
+/**
+ * Configures default exception handling using Ktor Status Pages.
+ *
+ * Returns following HTTP status codes:
+ * * 405 (Method Not Allowed) - when attempting to execute mutation or query through a GET request
+ * * 400 (Bad Request) - any other exception
+ */
 fun StatusPagesConfig.defaultGraphQLStatusPages(): StatusPagesConfig {
     exception<Throwable> { call, cause ->
         when (cause) {
