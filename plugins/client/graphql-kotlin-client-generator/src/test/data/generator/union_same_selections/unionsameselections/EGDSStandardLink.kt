00fa8add0edabbf58d2127f46cb3e4647980a2f6
Samuel Vazquez <sam_2f@hotmail.com>
2024-10-28 11:32:28 -0700

    feat: fix duplicated classes in packages and duplicated properties in constructors from interfaces (#2051) (#2052)
    ### :link: Related Issues
    https://github.com/ExpediaGroup/graphql-kotlin/issues/2050
    
    ---------
    
    Co-authored-by: Samuel Vazquez <samvazquez@expediagroup.com>

---
+++ plugins/client/graphql-kotlin-client-generator/src/test/data/generator/union_same_selections/unionsameselections/EGDSStandardLink.kt
@@ -0,0 +1,9 @@
+package com.expediagroup.graphql.generated.unionsameselections
+
+import com.expediagroup.graphql.client.Generated
+import kotlin.String
+
+@Generated
+public data class EGDSStandardLink(
+  public val text: String,
+)
---
+++ plugins/client/graphql-kotlin-client-generator/src/test/data/generator/union_same_selections/unionsameselections/ProductSupportingMessage.kt
@@ -0,0 +1,33 @@
+package com.expediagroup.graphql.generated.unionsameselections
+
+import com.expediagroup.graphql.client.Generated
+import com.fasterxml.jackson.`annotation`.JsonSubTypes
+import com.fasterxml.jackson.`annotation`.JsonTypeInfo
+import com.fasterxml.jackson.`annotation`.JsonTypeInfo.As.PROPERTY
+import com.fasterxml.jackson.`annotation`.JsonTypeInfo.Id.NAME
+import kotlin.String
+
+@Generated
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.NAME,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "__typename",
+  defaultImpl = DefaultProductSupportingMessageImplementation::class,
+)
+@JsonSubTypes(value = [com.fasterxml.jackson.annotation.JsonSubTypes.Type(value =
+    EGDSParagraph::class,
+    name="EGDSParagraph"),com.fasterxml.jackson.annotation.JsonSubTypes.Type(value =
+    EGDSPlainText::class, name="EGDSPlainText")])
+public interface ProductSupportingMessage
+
+@Generated
+public data class EGDSParagraph(
+  public val text: String,
+) : ProductSupportingMessage
+
+/**
+ * Fallback ProductSupportingMessage implementation that will be used when unknown/unhandled type is
+ * encountered.
+ */
+@Generated
+public class DefaultProductSupportingMessageImplementation() : ProductSupportingMessage
