4596b6d7d46262737c39bb055861e132478398a1
Samuel Vazquez <sam_2f@hotmail.com>
2024-10-28 11:32:28 -0700

    feat: fix duplicated classes in packages and duplicated properties in constructors from interfaces (#2051) (#2052)
    ### :link: Related Issues
    https://github.com/ExpediaGroup/graphql-kotlin/issues/2050
    
    ---------
    
    Co-authored-by: Samuel Vazquez <samvazquez@expediagroup.com>

---
+++ plugins/client/graphql-kotlin-client-generator/src/test/data/generator/union_same_selections/unionsameselections/EGDSProductRatingShowTextAction.kt
@@ -0,0 +1,9 @@
+package com.expediagroup.graphql.generated.unionsameselections
+
+import com.expediagroup.graphql.client.Generated
+import kotlin.String
+
+@Generated
+public data class EGDSProductRatingShowTextAction(
+  public val text: String,
+)
