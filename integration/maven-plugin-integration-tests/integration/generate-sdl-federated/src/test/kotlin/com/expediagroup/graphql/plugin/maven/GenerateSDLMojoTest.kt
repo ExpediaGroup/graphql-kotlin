4aa8ad2bb915155a14dbcc05696a524787d82a4e
Samuel Vazquez <samvazquez@expediagroup.com>
2025-05-02 10:45:15 -0700

    feat: update pugin tests and examples

--- integration/maven-plugin-integration-tests/integration/generate-sdl-federated/src/test/kotlin/com/expediagroup/graphql/plugin/maven/GenerateSDLMojoTest.kt
+++ integration/maven-plugin-integration-tests/integration/generate-sdl-federated/src/test/kotlin/com/expediagroup/graphql/plugin/maven/GenerateSDLMojoTest.kt
@@ -43,8 +43,11 @@ class GenerateSDLMojoTest {
             "Marks the field, argument, input field or enum value as deprecated"
             directive @deprecated(
                 "The reason for the deprecation"
-                reason: String = "No longer supported"
+                reason: String! = "No longer supported"
               ) on FIELD_DEFINITION | ARGUMENT_DEFINITION | ENUM_VALUE | INPUT_FIELD_DEFINITION
+
+            "This directive disables error propagation when a non nullable field returns null for the given operation."
+            directive @experimental_disableErrorPropagation on QUERY | MUTATION | SUBSCRIPTION
 
             "Directs the executor to include this field or fragment only when the `if` argument is true"
             directive @include(
