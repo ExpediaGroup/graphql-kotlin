fed2e86a5700b11a600cbd688991dab829878e86
Samuel Vazquez <samvazquez@expediagroup.com>
2026-02-25 12:22:49 -0800

    chore: update publishing to sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ tasks {
                 username.set(System.getenv("SONATYPE_USERNAME"))
                 password.set(System.getenv("SONATYPE_PASSWORD"))
                 nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-                snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             }
         }
 
